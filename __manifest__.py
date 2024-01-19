e376e243a9beb0b6d5b15503b2704917093d0792
alyraluca <alex@UbuntuAlex>
2024-01-19 17:48:01 +0100

    perfect

--- __manifest__.py
+++ __manifest__.py
@@ -2,16 +2,28 @@
 {
     'name': "planificacion_mod1",
 
-    'summary': """
+    'summary': "Simplifica la creacion de horarios.""
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
-    'description': """
+    'description': "Simplifica la creación de horarios y aumenta la productividad. Gestiona a la perfección los turnos y los recursos y disfruta de una nueva coordinación eficiente entre tus empleados.""
         Long description of module's purpose
-    """,
+    "TAB_Planeación:
+-	Podremos gestionar todos los turnos y organizarlos en tablas por puesto o por empleado.
+-	Podremos deja turnos sin asignar y asignarlos posteriormente con facilidad.
+-	Posibilidad de enviar a los empleados sus respectivos horarios.
+-	Posibilidad de copiar todos los turnos de la semana anterior y reorganizarlos.
+-	Planeación: mostrar los turnos en una tabla.
+-	Ver horarios por dia, semana, mes, año.
+TAB_Mi Horario
+-	Los empleados podran ver y gestionar sus horarios en una tabla.
+-	Turnos abiertos disponibles: los empleados pueden ver los turnos abiertos disponibles.
+-	Podremos verlo por dia, semana y més.
+TAB_Configuración
+-	Podremos crear turnos y puestos de trabajo con el boton de ‘CREAR’."",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': Alexandra Raluca, Savu",
+    'website': "https://github.com/alyraluca/planificacion_mod1.git",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
